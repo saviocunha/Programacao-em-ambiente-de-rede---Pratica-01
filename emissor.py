de964832ed1e31742559b6ae2c3322d1998a4a63
andretorre102 <andretorre@windowslive.com>
2026-07-01 22:22:38 -0300

    retirada dos comentários do professor, deixando apenas os da atividade

--- emissor.py
+++ emissor.py
@@ -3,9 +3,7 @@ import socket, struct, os, random
 IP_DESTINO = "127.0.0.1"
 PORTA_DESTINO = 8080
 
-# Coloque um arquivo PDF real na mesma pasta
-# altere o nome do arquivo de acordo com o código
-# ou altere o código de acordo com o nome do arquivo 
+
 
 
 NOME_ARQUIVO = "meu_arquivo.pdf"
@@ -16,8 +14,7 @@ FORMATO_CABECALHO = "!IIB"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Definindo o Timeout. Se recvfrom demorar mais de 1 segundo,
-# gera erro (exceção)
+# Definindo o Timeout - 1 segundo.
 
 sock.settimeout(1.0)
 
@@ -32,17 +29,14 @@ print(f'[*] ID da transação: {trans_id}')
 
 with open(NOME_ARQUIVO, 'rb') as arquivo:
     while True:
-        # Ler um pedaço do arquivo
         pedaco = arquivo.read(TAMANHO_PAYLOAD)
 
-        # Lógica para descobrir se é o último pacote
         flag = 1 if len(pedaco) < TAMANHO_PAYLOAD or not pedaco else 0
 
         if not pedaco and flag == 0:
-            break # Fim do arquivo
+            break 
         
 
-        # Montar o pacote
         cabecalho = struct.pack(FORMATO_CABECALHO, seq_num, trans_id, flag)
         pacote_completo = cabecalho + pedaco
 
@@ -79,7 +73,8 @@ with open(NOME_ARQUIVO, 'rb') as arquivo:
             except socket.timeout:
                 print(f'[!] Timeout! Não recebi o ACK do pacote {seq_num}.')
                 tentativas += 1
-
+            # Criação de uma exceção para lidar com o caso em que o receptor (servidor) 
+            # do lado do cliente não estiver mais disponível. 
             except ConnectionResetError:
                 print(f'[-] Erro: Conexão resetada pelo receptor. Tentando novamente...')
                 tentativas = MAX_TENTATIVAS  # Força a saída do loop para encerrar a transferência
--- receptor.py
+++ receptor.py
@@ -4,37 +4,27 @@ import struct
 import os
 import random
 
-# IP vazio ("") indica que o servidor irá escutar em todas as interfaces de rede disponíveis.
 IP = ""
 
-# Porta utilizada para receber os pacotes UDP.
 PORTA = 8080
 
-# Estrutura do cabeçalho:
-# !  -> ordem de bytes em rede (big-endian)
-# I  -> inteiro sem sinal de 4 bytes (número de sequência)
-# I  -> inteiro sem sinal de 4 bytes (ID da transação)
-# B  -> inteiro sem sinal de 1 byte (flag de controle)
+
 FORMATO_CABECALHO = "!IIB"
 
-# Calcula automaticamente o tamanho do cabeçalho em bytes.
 TAMANHO_CABECALHO = struct.calcsize(FORMATO_CABECALHO)
 
 
-# Criação do socket UDP (AF_INET = IPv4, SOCK_DGRAM = UDP)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Vincula o socket ao IP e Porta definidos
 sock.bind((IP, PORTA))
 
-sock.settimeout(10.0)  # fix: Define aqui um timeout de 10 segundos para operações de recebimento (Rollback)
+sock.settimeout(10.0)  #Define aqui um timeout de 10 segundos para operações de recebimento (Rollback)
 
 print(f'[*] Servidor UDP aguardando arquivos em {IP} : {PORTA}...')
 
-# Variáveis de controle de estado da transferência
-seq_esperado = 0  # Próximo número de sequência que o servidor aceitará
-arquivo_destino = open('recebido.pdf', 'wb')  # Cria/abre o arquivo final em modo binário | w -> write (escrever) b -> binary (binário)
-transacao_atual = None  # Guardará o ID da transferência ativa para evitar mistura de dados
+seq_esperado = 0  
+arquivo_destino = open('recebido.pdf', 'wb')  
+transacao_atual = None  
 
 
 try:
@@ -55,48 +45,41 @@ try:
             if os.path.exists("recebido.pdf"):
                 os.remove("recebido.pdf")  # Remove o arquivo incompleto
                 print("[-] Arquivo incompleto removido (Rollback).")
-            break  # Sai do loop principal, encerrando o servidor
+            break  
 
-        # Separa o cabeçalho dos dados reais (payload)
         cabecalho_bytes = pacote[:TAMANHO_CABECALHO]
         payload = pacote[TAMANHO_CABECALHO:]
 
-        # Desempacota o cabeçalho binário para variáveis do Python
         seq_num, trans_id, flag = struct.unpack(FORMATO_CABECALHO, cabecalho_bytes)
 
-        # Se for o primeiro pacote da sessão, registramos o trans_id atual
         if transacao_atual is None:
             transacao_atual = trans_id
 
-        # Verifica se o pacote pertence à transação atual
         if trans_id != transacao_atual:
-            continue # Ignora pacotes de outras sessões/clientes "alienígenas"
+            continue 
 
-        # Lógica de ordenação e ACK (Confirmação)
         if seq_num == seq_esperado:
             print(f"[+] Recebido pacote {seq_num}. Gravando no disco ...")
-            arquivo_destino.write(payload)  # Grava o pedaço do arquivo no disco
-            seq_esperado += 1  # Atualiza a sequência para o próximo bloco esperado
+            arquivo_destino.write(payload)  
+            seq_esperado += 1  
 
-            # Criar e enviar o ACK correspondente ao pacote recebido com sucesso
+          
             ack_pacote = struct.pack("!I", seq_num)
             sock.sendto(ack_pacote, endereco_cliente)
 
-            # Verificar se a flag indica que este é o último pacote do arquivo
+           
             if flag == 1:
                 print('[*] Último pacote recebido. Transferência concluída.')
-                arquivo_destino.close()  # Salva e fecha o arquivo com segurança
-                break # Sai do loop de recebimento
+                arquivo_destino.close() 
+                break 
         else:
-            # Se o pacote for antigo/duplicado, o cliente pode ter perdido o nosso ACK anterior.
-            # Reenviamos o ACK do pacote que o cliente mandou para tirá-lo do "limbo" de retransmissão.
+          
             if seq_num < seq_esperado:
                 ack_pacote = struct.pack("!I", seq_num)
                 sock.sendto(ack_pacote, endereco_cliente)
 
 # Essa parte aqui é o rollback em caso de fechamento abrupto do servidor
 except KeyboardInterrupt:
-    # Captura o Ctrl+C no terminal para encerrar o servidor
     print("\n [*] Servidor encerrado pelo usuário.")
     arquivo_destino.close()  # Garante que o arquivo não fique corrompido ao fechar
 
